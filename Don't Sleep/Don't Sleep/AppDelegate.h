15cc08b3ec25b81d63c2e0a7e9e907468ace9310
Farshid Besharati <farshidbt@gmail.com>
2014-03-25 11:07:37 +0100

    * Boiler plate project added.

---
+++ Don't Sleep/Don't Sleep/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Don't Sleep
+//
+//  Created by Farshid Besharati on 2014-03-25.
+//  Copyright (c) 2014 Don't Sleep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
