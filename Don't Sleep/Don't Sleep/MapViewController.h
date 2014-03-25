2cad0cf785b4323073c0d97a935f4f9b7b716930
Christian Danheimer Furedal <ChrillesMac@student250-59.eduroam.uu.se>
2014-03-25 11:28:57 +0100

    Added a ViewController with a MapView

---
+++ Don't Sleep/Don't Sleep/MapViewController.h
@@ -0,0 +1,14 @@
+//
+//  MapViewController.h
+//  Don't Sleep
+//
+//  Created by Christian Danheimer Furedal on 2014-03-25.
+//  Copyright (c) 2014 Don't Sleep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
