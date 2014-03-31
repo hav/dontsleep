3f1fbad64aed9b5e8ec42e578f5581c4c65a9aa2
Farshid Besharati <farshidbt@gmail.com>
2014-03-31 14:46:41 +0200

    * Added BaseModel

---
+++ QuizBro/QuizBro/GameModel/BaseModel.h
@@ -0,0 +1,14 @@
+//
+//  BaseModel.h
+//  QuizBro
+//
+//  Created by Farshid Besharati on 2014-03-31.
+//  Copyright (c) 2014 quizbro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BaseModel : NSObject
+
+
+@end
---
+++ QuizBro/QuizBro/GameModel/BaseModel.m
@@ -0,0 +1,30 @@
+//
+//  BaseModel.m
+//  QuizBro
+//
+//  Created by Farshid Besharati on 2014-03-31.
+//  Copyright (c) 2014 quizbro. All rights reserved.
+//
+
+#import "BaseModel.h"
+
+@interface BaseModel()
+
+@property (nonatomic, strong) NSArray *questions;
+
+@end
+
+@implementation BaseModel
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        
+    }
+    
+    return self;
+}
+
+
+@end
